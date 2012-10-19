03ad55e35471a7a3c674d4126e095421b6fa78c3
Ben Webb <bjwebb67@googlemail.com>
2012-10-19 21:28:02 +0100

    Add some install documentation

--- settings.py
+++ settings.py
@@ -22,14 +22,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'hunt',                      # Or path to database file if using sqlite3.
-        'USER': 'hunt',                      # Not used with sqlite3.
-        'PASSWORD': secret.PASSWORD,                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': secret.DATABASE 
 }
 
 # Local time zone for this installation. Choices can be found here:
