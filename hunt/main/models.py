2a2940561910c31ffe02a5e684757dde578bb33f
Ben Webb <bjwebb67@googlemail.com>
2013-06-13 13:33:18 +0100

    Tidy up some interface

--- hunt/main/models.py
+++ hunt/main/models.py
@@ -24,7 +24,7 @@ class Node(models.Model):
 class Puzzle(models.Model):
     name = models.CharField(max_length=256, default="")
     description = models.TextField(default="", blank=True)
-    node = models.OneToOneField(Node, null=True)
+    node = models.OneToOneField(Node, null=True, blank=True)
     solution = models.CharField(max_length=256, default="", blank=True)
     show_solution_box = models.BooleanField(default=True)
     function = models.TextField(default="", blank=True)
